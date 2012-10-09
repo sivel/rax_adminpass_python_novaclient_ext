7eefa09a2d91947a0aa4ff86cfa336589945b348
Matt Martz <matt@sivel.net>
2012-10-09 07:24:58 -0500

    Initial commit of os_adminpass_python_novaclient_ext

--- README.md
+++ README.md
@@ -2,3 +2,13 @@ os_adminpass_python_novaclient_ext
 ==================================
 
 Admin pass extension for python-novaclient
+
+Install
+-------
+
+    pip install git+git://github.com/sivel/os_adminpass_python_novaclient_ext.git
+
+Usage
+-----
+
+This extension will automatically be detected by novaclient and will provide the --admin-pass option for the boot command.
---
+++ setup.py
@@ -0,0 +1,36 @@
+# Copyright 2012 Matt Martz <matt@sivel.net>
+# All Rights Reserved.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+import setuptools
+
+
+setuptools.setup(
+    name="os_adminpass_python_novaclient_ext",
+    version="0.1",
+    author="Matt Martz",
+    author_email="matt@sivel.net",
+    description="Admin Pass extension for python-novaclient",
+    license="Apache License, Version 2.0",
+    packages=["os_adminpass_python_novaclient_ext"],
+    install_requires=["python-novaclient"],
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Information Technology",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+    ]
+)
